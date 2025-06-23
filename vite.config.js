4c848af4ac9df300420c08e06cc2b7cdf401b3b7
kishore-404 <kishores.developer@gmail.com>
2025-06-24 00:47:34 +0530

    changed the directory

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/your-subdirectory/',
    server: {
     hmr: {
       overlay: false, // Disable the HMR overlay (optional)
